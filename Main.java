9177b3eb2cba0b0b067e834e937801c1818664e0
YelloElefant <alextrotternz@gmail.com>
2024-03-18 01:35:50 +0000

    chanigng to 100 tickets to generate

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // StrLinkedList temp1 = new StrLinkedList();
-        // StrLinkedList temp2 = new StrLinkedList();
-        // System.out.println(temp1.test);
-        // System.out.println(temp2.test);
-        // temp1.test = 2;
-
-        // System.out.println(temp1.test);
-        // System.out.println(temp2.test);
+        StrLinkedList temp1 = new StrLinkedList();
+        StrLinkedList temp2 = new StrLinkedList();
+        System.out.println(temp1.test);
+        System.out.println(temp2.test);
+        temp1.test = 2;
+        System.out.println(temp1.test);
+        System.out.println(temp2.test);
 
     }
 }
